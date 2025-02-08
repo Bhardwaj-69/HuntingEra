64cb9ea0cc185dc36eadc97a75c841a93595ead1
Rockybhai21 <rockybhai2109rs@gmail.com>
2025-02-09 00:12:11 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -160,6 +160,19 @@ async def start(client, message):
             InlineKeyboardButton('📣 ᴊᴏɪɴ ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ 📣', url=CHNL_LNK)
         ]]
 
+    else:
+        buttons = [[
+            InlineKeyboardButton('🎀 ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ 💓', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+        ],[
+            InlineKeyboardButton('💰 ᴇᴀʀɴ ᴍᴏɴᴇʏ 💰', callback_data="shortlink_info"),
+            InlineKeyboardButton('📽 ᴍᴏᴠɪᴇ ɢʀᴏᴜᴘ 📽', url=GRP_LNK)
+        ],[
+            InlineKeyboardButton('⁉️ʜᴇʟᴘ ⁉️', callback_data='help'),
+            InlineKeyboardButton('🧸 ᴀʙᴏᴜᴛ 🧸', callback_data='about')
+        ],[
+            InlineKeyboardButton('📣 ᴊᴏɪɴ ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ 📣', url=CHNL_LNK)
+        ]]
+
         if CLONE_MODE == True:
             buttons.append([InlineKeyboardButton('ᴄʀᴇᴀᴛᴇ ᴏᴡɴ ᴄʟᴏɴᴇ ʙᴏᴛ', callback_data='clone')])
         reply_markup = InlineKeyboardMarkup(buttons)      
