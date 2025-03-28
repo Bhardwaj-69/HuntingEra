fd82053d3abf89e3ef254b8067ba248061980b9e
Rockybhai21 <rockybhai2109rs@gmail.com>
2025-03-28 11:01:30 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1534,7 +1534,7 @@ async def check_plans_cmd(client, message):
             [InlineKeyboardButton("⚠️ ᴄʟᴏsᴇ / ᴅᴇʟᴇᴛᴇ ⚠️", callback_data="close_data")]
         ]
         reply_markup = InlineKeyboardMarkup(btn)
-        m=await message.reply_sticker("CAACAgIAAxkBAAIBTGVjQbHuhOiboQsDm35brLGyLQ28AAJ-GgACglXYSXgCrotQHjibHgQ")         
+        m=await message.reply_sticker("CAACAgQAAxkBAAKPMWfmK10-bye00Udtl6ciL-EEZ7lKAAKTDQAC6yxoUB9HiKtXeWbiHgQ")         
         await message.reply_text(f"**😢 You Don't Have Any Premium Subscription.\n\n Check Out Our Premium /plan**",reply_markup=reply_markup)
         await asyncio.sleep(2)
         await m.delete()
