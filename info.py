23ca44a103a1670db5784e4793463d8b4b10cf3d
Rockybhai21 <rockybhai2109rs@gmail.com>
2025-03-28 14:04:13 +0530

    Update info.py

--- info.py
+++ info.py
@@ -24,7 +24,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 1800))
-PICS = (environ.get('PICS', 'https://envs.sh/g_q.jpg https://envs.sh/mXL.jpg https://envs.sh/AFo.jpg https://envs.sh/AFs.jpg')).split() #SAMPLE PIC
+PICS = (environ.get('PICS', 'https://envs.sh/g_q.jpg https://envs.sh/mXL.jpg https://postimg.cc/7G9NKhDD ')).split() #SAMPLE PIC
 NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/b69af2db776e4e85d21ec.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "https://t.me/How_To_Open_Linkl")
 SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5cbb8.jpg")
