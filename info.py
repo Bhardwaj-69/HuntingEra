88595573cbd5bf107305c40b3bbd2967889ed043
Bhardwaj-69 <crazyxl669@gmail.com>
2025-05-27 08:44:12 +0530

    Update info.py

--- info.py
+++ info.py
@@ -71,7 +71,7 @@ CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "") # Necessary If clone 
 PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Username Without @ or without https://t.me/ and Bot Is Admin With Full Right.
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/DM_HUB_69')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+wIfbcHf-MX44YWM1')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/LarvaLinks')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/PiratesHunts')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'MovieWalaChat') # Support Chat Link Without https:// or @
